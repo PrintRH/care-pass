eaeef88bcafb06cbc31e70ac221b987cd6bd87f0
DChan0319 <chaosstriker0319@gmail.com>
2023-10-17 14:16:30 -0700

    Moved login to it's own separate page for the web3AuthModalPack in /login

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,68 +1,27 @@
-import { useEffect, useState } from "react";
 import Link from "next/link";
-import {
-  Web3AuthEventListener, // SafeAuthKit,
-  // SafeAuthSignInData,
-  // SafeGetUserInfoResponse,
-  Web3AuthModalPack,
-} from "@safe-global/auth-kit";
-import {
-  ADAPTER_EVENTS, // SafeEventEmitterProvider,
-  // UserInfo,
-  // WALLET_ADAPTERS
-} from "@web3auth/base";
 import type { NextPage } from "next";
-import { instantiateWeb3AuthModalPack } from "~~/services/safe-protocol";
-
-const connectedHandler: Web3AuthEventListener = data => console.log("CONNECTED", data);
-const disconnectedHandler: Web3AuthEventListener = data => console.log("DISCONNECTED", data);
 
 const Home: NextPage = () => {
-  const [web3AuthModalPack, setWeb3AuthModalPack] = useState<Web3AuthModalPack>();
-  // const [safeAuthSignInResponse, setSafeAuthSignInResponse] = useState<AuthKitSignInData | null>(
-  //   null
-  // );
-  // const [userInfo, setUserInfo] = useState<Partial<UserInfo>>();
-  // const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(null);
-
-  useEffect(() => {
-    (async () => {
-      // TODO: this should be in the login portion.
-      const web3AuthModalPack = await instantiateWeb3AuthModalPack();
-      const authKitSignData = await web3AuthModalPack.signIn();
-      console.log("SIGN IN DATA:", authKitSignData);
-      console.log("web3AuthModal:", web3AuthModalPack);
-      console.log("web3AuthModal Provider:", web3AuthModalPack.getProvider());
-      web3AuthModalPack.subscribe(ADAPTER_EVENTS.CONNECTED, connectedHandler);
-      web3AuthModalPack.subscribe(ADAPTER_EVENTS.DISCONNECTED, disconnectedHandler);
-
-      setWeb3AuthModalPack(web3AuthModalPack);
-
-      return () => {
-        web3AuthModalPack.unsubscribe(ADAPTER_EVENTS.CONNECTED, connectedHandler);
-        web3AuthModalPack.unsubscribe(ADAPTER_EVENTS.DISCONNECTED, disconnectedHandler);
-      };
-    })();
-  }, [web3AuthModalPack]);
-
-  // const login = async () => {
-  //   if (!web3AuthModalPack) return;
-
-  //   const signInInfo = await web3AuthModalPack.signIn();
-  //   console.log("SIGN IN RESPONSE: ", signInInfo);
-
-  //   const userInfo = await web3AuthModalPack.getUserInfo();
-  //   console.log("USER INFO: ", userInfo);
-
-  //   setSafeAuthSignInResponse(signInInfo);
-  //   setUserInfo(userInfo || undefined);
-  //   setProvider(web3AuthModalPack.getProvider() as SafeEventEmitterProvider);
-  // };
-
   return (
-    <h1>
-      Hospital DAO <Link href="/Upload">Upload Here!</Link>
-    </h1>
+    <>
+      <h1>
+        Hospital DAO <Link href="/Upload">Upload Here!</Link>
+      </h1>
+      <div className="drawer drawer-open">
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-side">
+          <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+            {/* Sidebar content here */}
+            <Link href="/dashboard">Dashboard</Link>
+            <Link href="/profile">Profile</Link>
+            <Link href="/table">Table</Link>
+            <Link href="/login">Login</Link>
+            <Link href="/register">Register</Link>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 };
 
