bcf84c057c9d4deb9a3eedb6c6dd1dc6371c32d3
RH <renhau96@gmail.com>
2023-10-22 01:31:26 -0500

    update upload file function

--- packages/nextjs/pages/patient.tsx
+++ packages/nextjs/pages/patient.tsx
@@ -1,12 +1,38 @@
-import Link from "next/link";
+import Upload from "./upload";
 import { NextPage } from "next";
 
 const Patient: NextPage = () => {
   return (
     <div>
-      <Link href="/upload" passHref className="link">
-        File Upload
-      </Link>
+      <div className="mt-10 flex pl-6 gap-2 max-w-2xl">
+        <div className="flex gap-5 bg-black bg-opacity-80 z-0 rounded-2xl shadow-lg">
+          <span className="pl-4 pt-4 text-3xl">👋🏻</span>
+          <div>
+            <div className="pl-4 pt-4 text-white">Welcome back to Care Pass DAO.</div>
+            <span className="pl-4 pt-4 text-white leading-loose"> Upload your file below: </span>
+            <div className="p-6">
+              <button
+                type="button"
+                className="inline-block rounded bg-primary pl-6 pb-6 pt-6 text-s font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca]"
+                data-te-ripple-init
+                data-te-ripple-color="light"
+              >
+                <Upload />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <h2 className="pt-6 pl-6 pr-6 relative flex w-full flex-wrap items-stretch mb-3">
+        <span className="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300 absolute bg-transparent rounded text-lg items-center justify-center w-8 pl-3 py-3">
+          <i className="fas fa-lock"></i>
+        </span>
+        <input
+          type="text"
+          placeholder="Message Your Caregiver:"
+          className="px-3 py-4 placeholder-blueGray-300 text-black relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full pl-10"
+        />
+      </h2>
     </div>
   );
 };
